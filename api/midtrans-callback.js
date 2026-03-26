845f0ae9e60e17517cb627e604ece042c4d27955
RayanXweb <rohmanjakarta15@gmail.com>
2026-03-26 23:18:36 +0700

    Update midtrans-callback.js

--- api/midtrans-callback.js
+++ api/midtrans-callback.js
@@ -1,1 +1,19 @@
+const admin = require('../lib/firebase');
 
+module.exports = async (req, res) => {
+  const notification = req.body;
+
+  const status = notification.transaction_status;
+  const userId = notification.metadata?.userId;
+  const amount = parseInt(notification.gross_amount);
+
+  if (status === 'settlement') {
+    const userRef = admin.firestore().collection('users').doc(userId);
+
+    await userRef.update({
+      balance: admin.firestore.FieldValue.increment(amount)
+    });
+  }
+
+  res.send("OK");
+};
