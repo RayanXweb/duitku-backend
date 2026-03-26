051ed0313fb6d8fbbd947e1b8bb0e220a814a5ed
RayanXweb <rohmanjakarta15@gmail.com>
2026-03-26 23:18:13 +0700

    Update create-transaction.js

--- api/create-transaction.js
+++ api/create-transaction.js
@@ -1,1 +1,26 @@
+const midtransClient = require('midtrans-client');
 
+let snap = new midtransClient.Snap({
+  isProduction: false,
+  serverKey: process.env.MIDTRANS_SERVER_KEY
+});
+
+module.exports = async (req, res) => {
+  const { amount, userId } = req.body;
+
+  const parameter = {
+    transaction_details: {
+      order_id: "ORDER-" + Date.now(),
+      gross_amount: amount
+    },
+    metadata: {
+      userId: userId
+    }
+  };
+
+  const transaction = await snap.createTransaction(parameter);
+
+  res.json({
+    token: transaction.token
+  });
+};
